3f6030d261f37fb44d0343ce183510f8a889d8c5
murderteeth <murdertxxth@gmail.com>
2024-06-18 05:39:23 +0000

    Fix deps

--- app/Bg.tsx
+++ app/Bg.tsx
@@ -19,7 +19,7 @@ export default function Bg() {
       })
     }
     return result
-  }, [])
+  }, [xmax, ymax])
 
   return <div className={`
     fixed z-0 -top-[160px] -left-[160px] size-[2000px] 2xl:size-[2800px]`}>
