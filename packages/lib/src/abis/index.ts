ca424e9c7e7c98258b0e5025c7f90213f0d4c705
murderteeth <murdertxxth@gmail.com>
2024-09-24 18:13:27 +0000

    Upgrade viem\wagmi

--- packages/lib/src/abis/index.ts
+++ packages/lib/src/abis/index.ts
@@ -1,11 +1,13 @@
 import accountant from './accountant'
 import allocator from './allocator'
+import allocatorFactory from './allocatorFactory'
 import strategy from './strategy'
 import vault from './vault'
 
 const abis = { 
   accountant, 
   allocator,
+  allocatorFactory,
   strategy,
   vault 
 }
