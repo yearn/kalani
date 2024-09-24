97d6c2bd50df652d726e754bb5a98e33662d561b
murderteeth <murdertxxth@gmail.com>
2024-09-24 13:57:54 +0000

    Reland

--- packages/web/src/routes/lander/Bg.tsx
+++ packages/web/src/routes/lander/Bg.tsx
@@ -7,17 +7,17 @@ function PlaneteryDisc({ className }: { className: string }) {
 
 function Planet({ planeteryClassName, className }: { planeteryClassName: string, className?: string }) {
   return <div className={cn('absolute', planeteryClassName, className)}>
-    <PlaneteryDisc className={cn('top-[0px] left-[30px] bg-primary-800 blur-3xl', planeteryClassName)} />
-    <PlaneteryDisc className={cn('inset-0 bg-black', planeteryClassName)} />
+    <PlaneteryDisc className={cn('top-[0px] left-[30px] bg-primary-300 blur-3xl', planeteryClassName)} />
+    <PlaneteryDisc className={cn('inset-0 bg-primary-400', planeteryClassName)} />
   </div>
 }
 
 export default function Bg() {
   return <div className={`
-    isolate fixed -z-[1] inset-0 bg-primary-950`}>
+    isolate fixed -z-[1] inset-0 bg-primary-400`}>
     <div className="relative h-full w-full flex items-start justify-start overflow-hidden">
       <RetroGrid />
-      <div className="absolute w-full h-[50%] bg-black shadow-lg overflow-hidden">
+      <div className="absolute w-full h-[50%] bg-primary-400 overflow-hidden">
         <Planet planeteryClassName="-bottom-[600px] -left-[200px] size-[1120px] rotate-0" />
       </div>
     </div>
--- packages/web/src/routes/lander/Header.tsx
+++ packages/web/src/routes/lander/Header.tsx
@@ -4,7 +4,7 @@ import Launcher from '../../components/Launcher'
 export default function Header() {
   return <header
     className="absolute inset-x-0 top-0 z-50 w-full">
-    <div className="mx-auto max-w-6xl h-20 flex items-center justify-between">
+    <div className="mx-auto w-full h-20 pr-4 sm:pr-10 flex items-center justify-between">
       <div className="grow flex items-center justify-start gap-12"></div>
       <div className="flex items-center justify-end gap-4">
         <Connect />
