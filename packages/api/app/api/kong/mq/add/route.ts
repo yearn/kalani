7db42048104426e16b7ec5872cde8ac42d308bfe
murderteeth <murdertxxth@gmail.com>
2024-11-12 19:41:55 +0000

    Disable mq add ep

--- packages/api/app/api/kong/mq/add/route.ts
+++ packages/api/app/api/kong/mq/add/route.ts
@@ -10,15 +10,18 @@ export async function OPTIONS() {
 
 export async function POST(request: Request) {
   try {
-    const { queueName, jobName, data, options } = await request.json()
+    // const { queueName, jobName, data, options } = await request.json()
 
-    const response = await fetch(`${process.env.KONG_API}/mq/add`, {
-      method: 'POST', headers: { ...KONG_API_HEADERS },
-      body: JSON.stringify({ queueName, jobName, data, options })
-    })
+    // const response = await fetch(`${process.env.KONG_API}/mq/add`, {
+    //   method: 'POST', headers: { ...KONG_API_HEADERS },
+    //   body: JSON.stringify({ queueName, jobName, data, options })
+    // })
 
-    const { jobId } = await response.json()
-    return new Response(JSON.stringify({ queueName, jobId }), { headers })
+    // const { jobId } = await response.json()
+    // return new Response(JSON.stringify({ queueName, jobId }), { headers })
+
+    const { queueName } = await request.json()
+    return new Response(JSON.stringify({ queueName, jobId: '0' }), { headers })
 
   } catch (error) {
     console.error(error)
