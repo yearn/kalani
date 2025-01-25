216be08b71186f5f112725dbe9a13a355fda4bdf
murderteeth <murdertxxth@gmail.com>
2025-01-25 03:45:05 +0000

    Add default erc20s for sonic and mode chains

--- packages/web/src/components/SelectErc20/tokens.ts
+++ packages/web/src/components/SelectErc20/tokens.ts
@@ -19,6 +19,15 @@ export const TOKENS: Record<number, Erc20[]> = {
     { chainId: 137, address: '0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359', name: 'USDC', symbol: 'USDC', decimals: 6 },
     { chainId: 137, address: '0xc2132D05D31c914a87C6611C10748AEb04B58e8F', name: 'Tether USD', symbol: 'USDT', decimals: 18 }
   ],
+  [146]: [
+    { chainId: 146, address: '0x039e2fB66102314Ce7b64Ce5Ce3E5183bc94aD38', name: 'Wrapped Sonic', symbol: 'wS', decimals: 18 },
+    { chainId: 146, address: '0x50c42dEAcD8Fc9773493ED674b675bE577f2634b', name: 'Wrapper ETH', symbol: 'WETH', decimals: 18 },
+    { chainId: 146, address: '0x29219dd400f2Bf60E5a23d13Be72B486D4038894', name: 'Bridged USDC', symbol: 'USDC.e', decimals: 6 }
+  ],
+  [34443]: [
+    { chainId: 34443, address: '0x4200000000000000000000000000000000000006', name: 'Wrapper ETH', symbol: 'WETH', decimals: 18 },
+    { chainId: 34443, address: '0xd988097fb8612cc24eeC14542bC03424c656005f', name: 'USDC', symbol: 'USDC', decimals: 6 }
+  ],
   [42161]: [
     { chainId: 42161, address: '0x50c5725949A6F0c72E6C4a641F24049A917DB0Cb', name: 'Dai', symbol: 'DAI', decimals: 18 },
     { chainId: 42161, address: '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913', name: 'USDC', symbol: 'USDC', decimals: 6 }
