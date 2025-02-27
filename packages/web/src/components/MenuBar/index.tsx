afa80add2065fa1edb7e893b4734bf52ae9ba149
murderteeth <murdertxxth@gmail.com>
2025-02-27 23:02:57 +0000

    Use dialog for mobile menu

--- packages/web/src/components/MenuBar/index.tsx
+++ packages/web/src/components/MenuBar/index.tsx
@@ -1,16 +1,14 @@
-import { PiDotsNine, PiEqualsBold, PiMagnifyingGlass, PiRobot, PiVault, PiWallet, PiX } from 'react-icons/pi'
+import { PiDotsNine, PiEqualsBold, PiMagnifyingGlass, PiRobot, PiVault, PiWallet } from 'react-icons/pi'
 import { cn } from '../../lib/shadcn'
 import { useHashNav } from '../../hooks/useHashNav'
-import FlyInFromBottom from '../motion/FlyInFromBottom'
 import { IconType } from 'react-icons/lib'
 import Connect from '../Connect'
-import { useLocation, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import Launcher from '../Launcher'
-import Aside from '../../routes/dash/Aside'
 import { useMenuBar } from './useMenuBar'
 import { useState, useEffect } from 'react'
-
-const HIDE_ASIDE_FOR_ROUTES = ['/', '/explore', '/build', '/yhaas']
+import { useDialog } from '../Dialog/useDialog'
+import Dialog from '../Dialog'
 
 function MenuBarButton({ icon, title, onClick }: { icon: IconType, title: string, onClick: () => void }) {
   return <div onClick={onClick} className="w-full py-6 flex items-center active:bg-primary-600">
@@ -20,9 +18,8 @@ function MenuBarButton({ icon, title, onClick }: { icon: IconType, title: string
 }
 
 export default function MenuBar({ className }: { className?: string }) {
-  const location = useLocation()
   const navigate = useNavigate()
-  const menu = useHashNav('menu')
+  const menu = useDialog('menu')
   const launcher = useHashNav('launcher')
   const { theme } = useMenuBar()
   const [isScrolled, setIsScrolled] = useState(false)
@@ -36,39 +33,24 @@ export default function MenuBar({ className }: { className?: string }) {
   return <div className={cn('sm:hidden fixed z-50 inset-0 flex flex-col justify-start pointer-events-none', className)}>
 
     {<div className={`
-      w-full h-[4.5rem] px-6 flex flex-row items-center justify-between
+      w-full h-[4.5rem] pl-6 pr-3 flex flex-row items-center justify-between
       border-b-primary ${isScrolled ? 'border-black' : 'border-transparent'}
       pointer-events-none bg-grill-950`}>
-      <button onClick={menu.open} className={cn('pointer-events-auto', theme === 'warn' && 'text-warn-400 theme-warn')}>
+      <button onClick={menu.openDialog} className={cn('pointer-events-auto', theme === 'warn' && 'text-warn-400 theme-warn')}>
         <PiEqualsBold size={32} />
       </button>
       <Connect short={true} className="pointer-events-auto" />
     </div>}
 
-    {menu.isOpen && <FlyInFromBottom _key="menu-bar" className="relative grow bg-secondary-2000 pointer-events-auto overflow-auto">
-      <div className="px-6 py-6 flex items-center justify-end text-neutral-700">
-        <PiX size={32} onClick={menu.close} />
-      </div>
-
-      {!HIDE_ASIDE_FOR_ROUTES.includes(location.pathname) && <div className="mt-16">
-        <Aside />
-        <div className="w-full my-16 border-primary border-b border-black"></div>
-      </div>}
-
-      <div className="px-6 flex flex-col">
-        <Connect />
-      </div>
-
-      <div className="w-full my-16 border-primary border-b border-black"></div>
-
-      <div className="mb-32 flex flex-col items-start justify-center">
+    <Dialog dialogId={'menu'}>
+      <div className="-mx-6 flex flex-col items-start justify-center">
         <MenuBarButton icon={PiWallet} title="Account" onClick={() => navigate('/', { replace: true })} />
         <MenuBarButton icon={PiMagnifyingGlass} title="Explore" onClick={() => navigate('/explore', { replace: true })} />
         <MenuBarButton icon={PiVault} title="Build" onClick={() => navigate('/build', { replace: true })} />
         <MenuBarButton icon={PiRobot} title="yHaaS" onClick={() => navigate('/yhaas', { replace: true })} />
         <MenuBarButton icon={PiDotsNine} title="yEcosystem" onClick={launcher.open} />
       </div>
-    </FlyInFromBottom>}
+    </Dialog>
 
     <Launcher hideToggle={true} />
   </div>
