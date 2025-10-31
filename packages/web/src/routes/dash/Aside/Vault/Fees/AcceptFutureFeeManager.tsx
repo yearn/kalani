a773caa42d6787a7c5ce83f35b80ebb5141c75be
murderteeth <murdertxxth@gmail.com>
2025-10-31 05:45:37 +0000

    Fix accept fee manager confirmations

--- packages/web/src/routes/dash/Aside/Vault/Fees/AcceptFutureFeeManager.tsx
+++ packages/web/src/routes/dash/Aside/Vault/Fees/AcceptFutureFeeManager.tsx
@@ -19,7 +19,7 @@ function useAcceptFutureFeeManager() {
   }), [vault])
   const simulation = useSimulateContract(parameters)
   const { write, resolveToast } = useWriteContract()
-  const confirmation = useWaitForTransactionReceipt({ hash: write.data })
+  const confirmation = useWaitForTransactionReceipt({ hash: write.data, confirmations: 2 })
   return { simulation, write, confirmation, resolveToast }
 }
 
